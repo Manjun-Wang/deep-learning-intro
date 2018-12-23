a928c398bd0d48a6c90603aef9c8ee1dabedfb5c
Manjun <42054560+Manjun-Wang@users.noreply.github.com>
2018-12-23 17:23:45 -0500

    Update softmax.py

--- nn/softmax.py
+++ nn/softmax.py
@@ -5,3 +5,12 @@ def softmax(a):
   y = exp_a / sum_exp_a
   
   return y
+
+
+def softmax(a):
+  c = np.max(a)
+  exp_a = np.exp(a - c) # for over flow
+  sum_exp_a = np.sum(exp_a)
+  y = exp_a / sum_exp_a
+  
+  return y
